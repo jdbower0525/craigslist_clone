81de77350f511daef88b11e0c29ecb9e010d87c6
John D. Bower <jdbower0525@gmail.com>
2016-11-23 14:15:54 -0500

    stashing secrets

---
+++ craigslist_clone/secrets.py
@@ -0,0 +1,13 @@
+SECRET_KEY = 'i$5@@*%+k8vq9807t@)1!v(gx8t7b&-9ij9kx*-t4kgig+8nbb'
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'moviedatabase',
+        'USER': 'Bower',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
--- craigslist_clone/settings.py
+++ craigslist_clone/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i$5@@*%+k8vq9807t@)1!v(gx8t7b&-9ij9kx*-t4kgig+8nbb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,12 +72,6 @@ WSGI_APPLICATION = 'craigslist_clone.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
