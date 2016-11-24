9fbe0b71fe28b7fa0058c13be529b1262a864a63
John D. Bower <jdbower0525@gmail.com>
2016-11-24 14:57:08 -0500

    working on registration page

--- craigslist_clone/secrets.py
+++ craigslist_clone/secrets.py
@@ -4,7 +4,7 @@ SECRET_KEY = 'i$5@@*%+k8vq9807t@)1!v(gx8t7b&-9ij9kx*-t4kgig+8nbb'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'moviedatabase',
+        'NAME': 'cloneapp',
         'USER': 'Bower',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
